63a32d9a28b3abacb5a061047206518381a29f60
ChunYen <panchunyen@gmail.com>
2021-11-05 14:42:24 +0800

    add heapify function

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 using namespace std;
-
-void swap(int &a, int &b)
-{
-    int tmp;
-    tmp = a;
-    a = b;
-    b = tmp;
-}
+void swap(int &i, int &j);
+void heapify(int *iarr, int n, int i);
 
 int main()
 {
-    int a = 10, b = 5;
-    cout << "a:" << a << endl;
-    cout << "b:" << b << endl;
-    swap(a, b);
-    cout << "a:" << a << endl;
-    cout << "b:" << b << endl;
+    int arry[] = {0, 23, 65, 99};
+    heapify(arry, 3, 1);
+    for (int i = 1; i <= 3; i++)
+    {
+        cout << arry[i] << endl;
+    }
+
     return 0;
+}
+
+void swap(int &i, int &j)
+{
+    int tmp;
+    tmp = i;
+    i = j;
+    j = tmp;
+}
+
+void heapify(int *iarr, int n, int i)
+{
+    int dad = i;
+    int son = dad * 2;
+    while (son <= n) { 
+        if (son + 1 <= n && iarr[son] < iarr[son + 1])
+            son++;
+        if (iarr[dad] > iarr[son])
+            return;
+        else {
+            swap(iarr[dad], iarr[son]);
+            dad = son;
+            son = dad * 2 + 1;
+        }
+    }
 }
