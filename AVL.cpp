4fb056a9428f523c7ab6af1c9c647822e28dfd5a
ChunYen <panchunyen@gmail.com>
2021-11-12 14:29:29 +0800

    del unneeded text

--- AVL.cpp
+++ AVL.cpp
@@ -47,13 +47,11 @@ public:
  */
 int main()
 {
-    // int choice, item;
     int arry[] = {93, 76, 21, 33, 56, 43, 29, 171};
     avlTree avl;
     for (int item : arry)
-    {
         root = avl.insert(root, item);
-    }
+
     cout << "Balanced AVL Tree:" << endl;
     avl.display(root, 1);
     cout << endl;
@@ -66,58 +64,6 @@ int main()
     cout << "Postorder Traversal:" << endl;
     avl.postorder(root);
     cout << endl;
-
-    /* while (1)
-    {
-        cout << "\n---------------------" << endl;
-        cout << "AVL Tree Implementation" << endl;
-        cout << "\n---------------------" << endl;
-        cout << "1.Insert Element into the tree" << endl;
-        cout << "2.Display Balanced AVL Tree" << endl;
-        cout << "3.InOrder traversal" << endl;
-        cout << "4.PreOrder traversal" << endl;
-        cout << "5.PostOrder traversal" << endl;
-        cout << "6.Exit" << endl;
-        cout << "Enter your Choice: ";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter value to be inserted: ";
-            cin >> item;
-            root = avl.insert(root, item);
-            break;
-        case 2:
-            if (root == NULL)
-            {
-                cout << "Tree is Empty" << endl;
-                continue;
-            }
-            cout << "Balanced AVL Tree:" << endl;
-            avl.display(root, 1);
-            break;
-        case 3:
-            cout << "Inorder Traversal:" << endl;
-            avl.inorder(root);
-            cout << endl;
-            break;
-        case 4:
-            cout << "Preorder Traversal:" << endl;
-            avl.preorder(root);
-            cout << endl;
-            break;
-        case 5:
-            cout << "Postorder Traversal:" << endl;
-            avl.postorder(root);
-            cout << endl;
-            break;
-        case 6:
-            exit(1);
-            break;
-        default:
-            cout << "Wrong Choice" << endl;
-        }
-    } */
     return 0;
 }
 
