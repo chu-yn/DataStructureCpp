57e3dec34f8ac5c6f22b9e6a71ecb1e7a45d4aea
chu-yn <panchunyen@gmail.com>
2022-01-07 07:33:46 +0000

    complete

--- hash_search.cpp
+++ hash_search.cpp
@@ -15,3 +15,52 @@ public:
         next = nullptr;
     }
 };
+
+class hash_linklist
+{
+public:
+    Node index_table[imax];
+    void Add_data(int val)
+    {
+        Node *newnode = new Node(val);
+        int hash = val % 7;
+        Node *current = &index_table[hash];
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+        current->next = newnode;
+    }
+
+    void print_data()
+    {
+        Node *head;
+        for (int i = 0; i < imax; i++)
+        {
+            cout << "hash[" << i << "]:";
+            if (index_table[i].next != nullptr)
+            {
+                head = index_table[i].next;
+                do
+                {
+                    cout << "[" << head->val << "]";
+                    head = head->next;
+                } while (head != nullptr);
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    hash_linklist list;
+    int arry[] = {93, 76, 21, 33, 56, 43, 29, 171, 10, 15, 11, 89, 99, 100, 77};
+    for (int item : arry)
+    {
+        list.Add_data(item);
+    }
+    list.print_data();
+
+    return 0;
+}
